9b74368f9a8b233e35451178ed56e3e4aec63875
chris bird <christopher.bird@climaterisk.com.au>
2023-12-12 14:39:03 +1100

    add cube

---
+++ src/objects.js
@@ -0,0 +1,60 @@
+import * as THREE from 'three'
+
+
+export const addRing = () => {
+    const vertices = [];
+    const divisions = 10;
+
+    for ( let i = 0; i <= divisions; i ++ ) {
+
+        const v = ( i / divisions ) * ( Math.PI * 2 );
+
+        const x = Math.sin( v );
+        const z = Math.cos( v );
+
+        vertices.push( x, 0, z );
+
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    //
+
+    const material = new THREE.LineBasicMaterial( {
+        color: 'red',
+        linewidth: 1,
+    } );
+
+    return new THREE.Line( geometry, material );
+
+}
+
+export const addCalibrationPoint = () => {
+    const vertices = [];
+    const divisions = 2;
+
+    for ( let i = 0; i <= divisions; i ++ ) {
+
+        const v = ( i / divisions ) * ( Math.PI * 2 );
+
+        const x = Math.sin( v );
+        const z = Math.cos( v );
+
+        vertices.push( x, 0, z );
+
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    //
+
+    const material = new THREE.LineBasicMaterial( {
+        color: 'red',
+        linewidth: 1,
+    } );
+
+    return new THREE.Line( geometry, material );
+
+};
--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
 import './style.css'
 import * as THREE from 'three'
 import { SVGRenderer } from 'three/examples/jsm/renderers/SVGRenderer';
+import CANNON from 'cannon'
 
 import * as dat from 'lil-gui'
 
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
+
+
 const parse = require('parse-svg-path');
 
 
 const helpers = require("./helpers");
+const objects = require("./objects");
 
 
 THREE.ColorManagement.enabled = false;
@@ -18,6 +22,12 @@ let camera, scene, renderer, rws, line, calibration;
 
 const ENABLED_WEBSOCKETS = true;
 const LASER_WC_URL = 'ws://localhost:8321';
+
+/**
+ * Utils
+ */
+
+const objectsToUpdate = []
 
 
 /**
@@ -42,73 +52,105 @@ const sizes = {
     height: 1080
 }
 
+
+/**
+ * Physics
+ */
+const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
+world.gravity.set(0, - 9.82, 0)
+
+// Material
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.7
+    }
+)
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
+
+// Create box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+})
+const createBox = (width, height, depth, position) =>
+{
+    // Three.js mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial,
+        // collisionFilterGroup: 0,
+        // collisionFilterMask: 0
+    })
+    body.position.copy(position)
+    // body.addEventListener('collide', playHitSound)
+    world.addBody(body)
+
+    // Save in objects
+    objectsToUpdate.push({ mesh, body })
+}
+
+buttons.createBox = () =>
+{
+    createBox(
+        0.5,
+        0.5,
+        0.5,
+        {
+            x: 0, // left to right
+            y: 3,
+            z: 0, // depth (not really needed)
+        }
+    )
+}
+gui.add(buttons, 'createBox')
+
+
+// Add Star
+const addStar = () => {
+
+    const geometry = new THREE.PlaneGeometry( 1, 1 );
+    const material = new THREE.MeshBasicMaterial( {color: 'red', side: THREE.DoubleSide} );
+    return new THREE.Mesh( geometry, material );
+}
+
+
+buttons.createStar = () =>
+{
+    addStar()
+}
+gui.add(buttons, 'createStar')
+
+
 init();
-animate();
+// animate();
 
-function addRing() {
-    const vertices = [];
-    const divisions = 10;
 
-    for ( let i = 0; i <= divisions; i ++ ) {
-
-        const v = ( i / divisions ) * ( Math.PI * 2 );
-
-        const x = Math.sin( v );
-        const z = Math.cos( v );
-
-        vertices.push( x, 0, z );
-
-    }
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-    //
-
-    const material = new THREE.LineBasicMaterial( {
-        color: 'red',
-        linewidth: 1,
-    } );
-
-    return new THREE.Line( geometry, material );
-
-}
-
-function addCalibrationPoint() {
-    const vertices = [];
-    const divisions = 2;
-
-    for ( let i = 0; i <= divisions; i ++ ) {
-
-        const v = ( i / divisions ) * ( Math.PI * 2 );
-
-        const x = Math.sin( v );
-        const z = Math.cos( v );
-
-        vertices.push( x, 0, z );
-
-    }
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-    //
-
-    const material = new THREE.LineBasicMaterial( {
-        color: 'red',
-        linewidth: 1,
-    } );
-
-    return new THREE.Line( geometry, material );
-
-}
 
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 87) {        // 'W'
-        line.position.y -= 0.5
+        line.position.y += 0.5
     } else if (keyCode == 83) { // 'S'
-        line.position.y += 0.5;
+        line.position.y -= 0.5;
     } else if (keyCode == 65) { // 'A'
         line.position.x -= 0.5; 
     } else if (keyCode == 68) { // 'D'  
@@ -140,12 +182,12 @@ function init() {
     document.body.appendChild( renderer.domElement );
 
     // RING
-    line = addRing();
+    line = objects.addRing();
     line.scale.setScalar( 0.5 );
     scene.add( line );
     
     // CALIBRATION
-    calibration = addCalibrationPoint();
+    calibration = objects.addCalibrationPoint();
     calibration.scale.setScalar( 0.05 );
     calibration.position.x = -2.9;
     calibration.position.y = 2.9;
@@ -165,24 +207,47 @@ function onWindowResize() {
 
 }
 
-function animate() {
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let oldElapsedTime = 0
 
-    let count = 0;
-    const time = performance.now() / 1000;
+const tick = () =>
+{
 
-    scene.traverse( function ( child ) {
+    // update physics world
 
-        // child.rotation.x = count + ( time / 3 );
-        // child.rotation.z = count + ( time / 4 );
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
 
-        count ++;
+    // Update physics
+    world.step(1 / 60, deltaTime, 3)
 
-    } );
 
-    renderer.render( scene, camera );
-    requestAnimationFrame( animate );
+    for(const object of objectsToUpdate)
+    {
+        object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
+    }
 
+    // Rotate the cube
+    line.rotation.x += 0.01;
+    line.rotation.z += 0.01;
+
+
+    // Update controls  
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
+
+tick()
 
 
 // Function to send a message to the WebSocket server
@@ -235,5 +300,5 @@ const sendSvgToLaser = () => {
 
 
 if (ENABLED_WEBSOCKETS) {
-    // setInterval(sendSvgToLaser, 50);
+    setInterval(sendSvgToLaser, 50);
 }
